3c83a1db499604b586d91791713326937aa10941
yogeshyogi634 <yogeshyogi634@gmail.com>
2024-07-26 20:12:25 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+    </div>
+  );
+}
+
+export default App;

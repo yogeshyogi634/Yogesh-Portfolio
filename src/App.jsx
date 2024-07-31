b1b869a7c35f2f33ab2aa530adb2a8443a89e032
yogeshyogi634 <yogeshyogi634@gmail.com>
2024-07-31 19:55:00 +0530

    files added

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Skills from "./components/skills/Skills";
 
 function App() {
   return (
-    <div>
+    <section>
       <Cursor />
       <section id="Home">
         <Navbar />
@@ -28,7 +28,7 @@ function App() {
       <section id="Contact">
         <Contact />
       </section>
-    </div>
+    </section>
   );
 }
 
